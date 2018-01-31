8641c21863c18e304c64880779d2a4de5ad28081
Corey <developer.coreycaplan@gmail.com>
2018-01-30 19:23:36 -0500

    Added UI for encryption

---
+++ app/src/main/kotlin/com/caplaninnovations/looprwallet/activities/WelcomeActivity.kt
@@ -0,0 +1,21 @@
+package com.caplaninnovations.looprwallet.activities
+
+import android.os.Bundle
+import com.caplaninnovations.looprwallet.R
+
+/**
+ *  Created by Corey on 1/30/2018.
+ *  Project: loopr-wallet-android
+ * <p></p>
+ *  Purpose of Class:
+ */
+class WelcomeActivity : BaseActivity() {
+
+    override val contentView: Int
+        get() = R.layout.activity_welcome
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+}
