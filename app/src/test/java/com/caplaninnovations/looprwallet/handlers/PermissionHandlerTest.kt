b53b358330ffd08bed0472c3072b404c99b06c2a
Corey <developer.coreycaplan@gmail.com>
2018-02-10 00:58:19 -0500

    Adding testing for permission handler

---
+++ app/src/test/java/com/caplaninnovations/looprwallet/handlers/PermissionHandlerTest.kt
@@ -0,0 +1,25 @@
+package com.caplaninnovations.looprwallet.handlers
+
+import org.junit.Test
+
+import org.junit.Assert.*
+
+/**
+ * Created by Corey on 2/9/2018.
+ *
+ *
+ * Project: loopr-wallet-android
+ *
+ *
+ * Purpose of Class:
+ */
+class PermissionHandlerTest {
+    @Test
+    fun requestPermission() {
+    }
+
+    @Test
+    fun onRequestPermissionsResult() {
+    }
+
+}
