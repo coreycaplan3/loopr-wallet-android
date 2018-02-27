c92614fbc8594600aa2df66a237b93f9653785e4
Corey Caplan <developer.coreycaplan@gmail.com>
2018-02-27 17:30:10 -0500

    Finished basic ETH transaction view model

--- app/src/main/kotlin/com/caplaninnovations/looprwallet/viewmodels/EthereumTransactionViewModel.kt
+++ app/src/main/kotlin/com/caplaninnovations/looprwallet/viewmodels/EthereumTransactionViewModel.kt
@@ -5,6 +5,7 @@ import android.arch.lifecycle.MutableLiveData
 import android.arch.lifecycle.ViewModel
 import com.caplaninnovations.looprwallet.application.LooprWalletApp
 import org.web3j.crypto.Credentials
+import org.web3j.protocol.core.methods.response.TransactionReceipt
 import org.web3j.tx.Transfer
 import org.web3j.utils.Convert
 import java.math.BigDecimal
@@ -19,12 +20,31 @@ import java.math.BigDecimal
  */
 class EthereumTransactionViewModel : ViewModel() {
 
-    private val isTransactionRunning: LiveData<Boolean> = MutableLiveData<Boolean>()
+    val isTransactionRunning = MutableLiveData<Boolean>()
+
+    val transactionResult = MutableLiveData<TransactionReceipt>()
+
+    val transactionError = MutableLiveData<Throwable>()
 
     fun sendEthereum(credentials: Credentials, recipient: String, amount: BigDecimal) {
+        isTransactionRunning.value = true
+
         val ethereumClient = LooprWalletApp.application.ethereumClient
-        val future = Transfer.sendFunds(ethereumClient, credentials, recipient, amount, Convert.Unit.ETHER)
-                .sendAsync()
+        Transfer.sendFunds(ethereumClient, credentials, recipient, amount, Convert.Unit.ETHER)
+                .observable()
+                .subscribe(this::onEthereumSendSuccess, this::onEthereumSendFailure)
+    }
+
+    // MARK - Private Methods
+
+    private fun onEthereumSendSuccess(receipt: TransactionReceipt) {
+        isTransactionRunning.value = false
+        transactionResult.value = receipt
+    }
+
+    private fun onEthereumSendFailure(e: Throwable) {
+        isTransactionRunning.value = false
+        transactionError.value = e
     }
 
 }
